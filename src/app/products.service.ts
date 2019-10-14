87e92ede6ecb5463eddad6c1a82096274debfd23
Hitesh20 <hiteshmunjal22@gmail.com>
2019-10-14 22:26:52 +0530

    some small changes

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -13,29 +13,24 @@ export class ProductsService {
   private url = 'http://localhost:2019';
 
   getAllProducts(): Observable<ProductClass[]> {
-    const headers = new HttpHeaders({Authorization: sessionStorage.getItem('basicAuth')});
-    return this.http.get<ProductClass[]>(this.url + '/products', {headers});
+    return this.http.get<ProductClass[]>(this.url + '/products');
   }
 
   // tslint:disable-next-line:ban-types
   getOneProduct(id: Number): Observable<ProductClass> {
-    const headers = new HttpHeaders({Authorization: sessionStorage.getItem('basicAuth')});
-    return this.http.get<ProductClass>(this.url + '/product-detail/' + id, {headers});
+    return this.http.get<ProductClass>(this.url + '/product-detail/' + id);
   }
 
   getProductsOfCategory(category: string): Observable<ProductClass[]> {
-    const headers = new HttpHeaders({Authorization: sessionStorage.getItem('basicAuth')});
-    return this.http.get<ProductClass[]>(this.url + '/productsFrom/' + category, {headers});
+    return this.http.get<ProductClass[]>(this.url + '/productsFrom/' + category);
   }
 
   getProductsOfCategoryAndPrice(category: string, price1: number, price2: number): Observable<ProductClass[]> {
-    const headers = new HttpHeaders({Authorization: sessionStorage.getItem('basicAuth')});
-    return this.http.get<ProductClass[]>(this.url + '/products/' + category + '/' + price1 + '/' + price2, {headers});
+    return this.http.get<ProductClass[]>(this.url + '/products/' + category + '/' + price1 + '/' + price2);
   }
 
   getAllProductsOfPrice(price1: number, price2: number): Observable<ProductClass[]> {
-    const headers = new HttpHeaders({Authorization: sessionStorage.getItem('basicAuth')});
-    return this.http.get<ProductClass[]>(this.url + '/products/' + price1 + '/' + price2, {headers});
+    return this.http.get<ProductClass[]>(this.url + '/products/' + price1 + '/' + price2);
   }
 
   addProduct(product) {
